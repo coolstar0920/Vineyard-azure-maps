1e7ced4a008b0fcc86d1e1cd8fcb48e8ad00db1f
Josip <110814772+coolstar0920@users.noreply.github.com>
2022-08-10 14:43:50 +0300

    Update Add section

--- src/components/Styled.js
+++ src/components/Styled.js
@@ -1,5 +1,6 @@
 import React from "react";
 import InputBase from "@mui/material/InputBase";
+import TextField from '@mui/material/TextField';
 import { styled } from '@mui/material/styles';
 
 export const CInput = styled((props) => (
@@ -9,4 +10,23 @@ export const CInput = styled((props) => (
     borderRadius: 6,
     border: '1px solid rgb(118, 118, 118)',
     padding: 12,
+}))
+
+export const CTextField = styled((props) => (
+    <TextField {...props} size="small" />
+))(() => ({
+    // '& .MuiTextField-root': {
+    //     height: 45,
+    //     '& .MuiInputBase-root': {
+    //         borderRadius: 6,
+    //         border: '1px solid rgb(118, 118, 118)',
+    //     }
+    // },
+    // '& label': {
+    //     lineHeight: 1
+    // },
+    // "& input": {
+
+    //     padding: 12,
+    // }
 }))
